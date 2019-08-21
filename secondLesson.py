050817745fab5fc806899fc8c7d3633a9151e02c
wafa <wafaharbi@gmail.com>
2019-08-21 21:01:47 +0300

    Initial commit for lesson two

---
+++ secondLesson.py
@@ -0,0 +1,9 @@
+
+# __________ second Challenges in 100 Programming code ____________#
+
+print("Hello World!") # Tis is a comment
+
+# This is a comment
+#  Written in
+#  more then just one line
+print("Cheers , Mate!")
